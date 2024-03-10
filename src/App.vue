7b037c58b054503f438e61b51968527100a48f9a
manhdv16 <manhvp0412@gmail.com>
2024-03-10 17:03:27 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@ export default {
   data() {
     return {
       // baseURL: "https://bebookstore-production.up.railway.app/",
-      baseURL: "http://localhost:8088/",
+      baseURL: "https://bebookstore-production-6e3c.up.railway.app/",
+      // baseURL: "http://localhost:8088/",
       books: null,
       categories: null,
     };
